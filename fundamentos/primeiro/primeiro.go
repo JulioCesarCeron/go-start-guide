f475fb7ad60ad1070b64f2e049d30eb4aff2e8c7
Julio Cesar Ceron <julio.ceron@mercadolivre.com>
2021-07-30 22:15:35 -0300

    First program

--- fundamentos/primeiro/primeiro.go
+++ fundamentos/primeiro/primeiro.go
@@ -1,1 +1,12 @@
 package primeiro
+
+import "fmt"
+
+/*
+Os códigos em GO são organizados em pacotes
+e para usá-los é necessário declarar um ou vários imports
+*/
+func main() {
+	fmt.Print("Primeiro ")
+	fmt.Println("Programa")
+}
